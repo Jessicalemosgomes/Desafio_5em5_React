cd36fca7a0d745402006fcb3b1b52ae370b03bf1
jessica lemos gomes <lemosgomes24@gmail.com>
2021-12-03 23:35:55 -0300

    Adicionando componente TableItem.

--- Sistema de Finanças Pessoais/sistema/src/helpers/dateFilter.ts
+++ Sistema de Finanças Pessoais/sistema/src/helpers/dateFilter.ts
@@ -20,3 +20,19 @@ export const filterListByMonth = (list: Item[], date: string): Item[] => {
     
     return newList;
 }
+
+export const formatDate = (date:Date): string => {
+    let year = date.getFullYear();
+    let month = date.getMonth() +1;
+    let day = date.getDate();
+
+    return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`;
+}
+
+const addZeroToDate = (n: number): string => {
+    if(n < 10){
+        return `0${n}`;
+    } else {
+        return `${n}`;
+    }
+}
