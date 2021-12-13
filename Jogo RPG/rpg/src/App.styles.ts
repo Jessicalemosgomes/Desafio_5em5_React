c92acdbce266a300ccc82a4fc7087ecb8cd2f71f
jessica lemos gomes <lemosgomes24@gmail.com>
2021-12-13 16:09:33 -0300

    Adicionando pastas assests e data ao jogo rpg.

---
+++ Jogo RPG/rpg/src/App.styles.ts
@@ -0,0 +1,15 @@
+import * as C from './App.styles';
+
+const App = () => {
+   return (
+    <C.Container>
+    <C.Map>
+    
+    
+    </C.Map>
+    </C.Container>
+  );
+
+}
+
+export default App;
---
+++ Jogo RPG/rpg/src/App.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #24282F;
+  min-height: 100vh;
+  color: #FFF;
+`;
